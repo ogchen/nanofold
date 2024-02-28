7d2d10d329608bc17634766d07af9c9b1ad63a06
Oscar Chen <oscarguoyinchen@gmail.com>
2024-02-28 17:32:44 +0000

    Add helper functions to compute rotation matrix of a residue

---
+++ nanofold/residue.py
@@ -0,0 +1,39 @@
+import torch
+
+RESIDUE_LIST = [
+    ("A", "ALA"),
+    ("R", "ARG"),
+    ("N", "ASN"),
+    ("D", "ASP"),
+    ("C", "CYS"),
+    ("Q", "GLN"),
+    ("E", "GLU"),
+    ("G", "GLY"),
+    ("H", "HIS"),
+    ("I", "ILE"),
+    ("L", "LEU"),
+    ("K", "LYS"),
+    ("M", "MET"),
+    ("F", "PHE"),
+    ("P", "PRO"),
+    ("S", "SER"),
+    ("T", "THR"),
+    ("W", "TRP"),
+    ("Y", "TYR"),
+    ("V", "VAL"),
+]
+
+
+def compute_residue_rotation(n_coords, ca_coords, c_coords):
+    ca_c = c_coords - ca_coords
+    ca_n = n_coords - ca_coords
+    ca_c_unit = ca_c / ca_c.norm()
+    ca_n_unit = ca_n / ca_n.norm()
+    cross = torch.linalg.cross(ca_c_unit, ca_n_unit)
+    cross_unit = cross / cross.norm()
+
+    reference_mat = torch.tensor(
+        [[1, 0, 0], [ca_c_unit.dot(ca_n_unit), cross.norm(), 0], [0, 0, 1]]
+    ).transpose(-2, -1)
+    mat = torch.stack([ca_c_unit, ca_n_unit, cross_unit]).transpose(-2, -1)
+    return mat @ reference_mat.inverse()
---
+++ tests/test_residue.py
@@ -0,0 +1,11 @@
+from nanofold import residue
+import torch
+import math
+
+
+def test_compute_residue_rotation():
+    n = torch.tensor([101.601, 38.534, -1.962])
+    ca = torch.tensor([103.062, 38.513, -2.159])
+    c = torch.tensor([103.354, 38.323, -3.656])
+    result = residue.compute_residue_rotation(n, ca, c)
+    assert torch.allclose(result @ result.transpose(-2, -1), torch.eye(3), atol=1e-5)
