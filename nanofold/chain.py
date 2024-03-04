55b7a4091bc95173bd7e59f3f5fbac8ce06878cf
Oscar Chen <oscarguoyinchen@gmail.com>
2024-03-04 17:09:37 +0000

    Add len to chain

--- nanofold/chain.py
+++ nanofold/chain.py
@@ -14,6 +14,12 @@ class Chain:
         if len(self.sequence) != len(self.chain):
             raise RuntimeError(f"Sequence length mismatch for chain {self.id}")
 
+    def __repr__(self):
+        return f"Chain(id={self.id}, sequence={self.sequence})"
+
+    def __len__(self):
+        return len(self.chain)
+
     def add_residue(self, residue):
         self.chain.append({"resname": residue["resname"], "id": residue["id"]})
         self.frames += Frame(residue["rotation"], residue["translation"])
