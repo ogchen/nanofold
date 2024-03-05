c1df7f24729303cd2f9434552280417a8f9b4dc5
Oscar Chen <oscarguoyinchen@gmail.com>
2024-03-05 17:51:50 +0000

    Add positions to chain

--- nanofold/chain.py
+++ nanofold/chain.py
@@ -5,15 +5,16 @@ from nanofold.residue import RESIDUE_LIST
 class Chain:
     RESIDUE_LOOKUP = {r[1]: r[0] for r in RESIDUE_LIST}
 
-    def __init__(self, id, chain, frames, sequence):
+    def __init__(self, id, chain, frames, sequence, positions):
         self.id = id
         self.chain = chain
         self.frames = frames
         self.sequence = sequence
+        self.positions = positions
 
     @classmethod
     def from_residue_list(cls, id, residue_list):
-        chain = cls(id, chain=[], frames=Frame(), sequence="")
+        chain = cls(id, chain=[], frames=Frame(), sequence="", positions=[])
         for residue in residue_list:
             chain.add_residue(residue)
         return chain
@@ -26,7 +27,7 @@ class Chain:
 
     def __getitem__(self, key):
         if isinstance(key, slice):
-            return Chain(self.id, self.chain[key], self.frames[key], self.sequence[key])
+            return Chain(self.id, self.chain[key], self.frames[key], self.sequence[key], self.positions[key])
 
     def add_residue(self, residue):
         self.chain.append(
@@ -37,3 +38,4 @@ class Chain:
         )
         self.frames += Frame(residue["rotation"], residue["translation"])
         self.sequence += Chain.RESIDUE_LOOKUP[residue["resname"]]
+        self.positions.append(residue["id"][-1][1])
