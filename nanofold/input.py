78e796d2f6fbe547b61d2fe37e2bb2d5fb981966
Oscar Chen <oscarguoyinchen@gmail.com>
2024-03-05 17:47:26 +0000

    Add input embedder

---
+++ nanofold/input.py
@@ -0,0 +1,38 @@
+import torch
+from torch import nn
+from nanofold.residue import RESIDUE_LIST
+
+
+def encode_one_hot(seq):
+    """Convert a sequence to one-hot encoding."""
+    res_map = {res[0]: i for i, res in enumerate(RESIDUE_LIST)}
+    seq = seq.upper()
+    one_hot = torch.zeros(len(seq), len(res_map))
+    for i, residue in enumerate(seq):
+        one_hot[i, res_map[residue]] = 1
+    return one_hot
+
+
+class InputEmbedder(nn.Module):
+    def __init__(self, embedding_size, position_bins):
+        super().__init__()
+        self.embedding_size = embedding_size
+        self.bins = position_bins
+        input_size = len(RESIDUE_LIST)
+        self.linear_a = nn.Linear(input_size, self.embedding_size)
+        self.linear_b = nn.Linear(input_size, self.embedding_size)
+        self.linear_position = nn.Linear(2*self.bins + 1, self.embedding_size)
+
+    def forward(self, target_feat, residue_index):
+        a = self.linear_a(target_feat)
+        b = self.linear_b(target_feat)
+        # Add a dimension to a and b to allow broadcasting
+        a = a.reshape(-1, 1, self.embedding_size)
+        b = b.reshape(1, -1, self.embedding_size)
+        z = a + b
+
+        d = residue_index.reshape(-1, 1) - residue_index
+        d = d.clamp(min=-self.bins, max=self.bins) + self.bins
+        p = nn.functional.one_hot(d)
+        p = self.linear_position(p.float())
+        return z + p
---
+++ tests/test_input.py
@@ -0,0 +1,32 @@
+from nanofold import input
+import torch
+
+
+def test_encode_one_hot():
+    seq = "ADHIAA"
+    one_hot = input.encode_one_hot(seq)
+    expected = torch.tensor(
+        [
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+    assert torch.equal(one_hot, expected)
+
+
+@torch.no_grad
+def test_input_embedder():
+    embedding_size = 5
+    position_bins = 3
+    embedder = input.InputEmbedder(embedding_size, position_bins)
+
+    seq = "ADHIAAAA"
+    target_feat = input.encode_one_hot(seq)
+    residue_index = torch.arange(len(seq))
+
+    x = embedder(target_feat, residue_index)
+    assert x.shape == (len(seq), len(seq), embedding_size)
