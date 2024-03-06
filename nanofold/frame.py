4ae06b3e1e09ab1e71233f379dde8aa0542897fa
Oscar Chen <oscarguoyinchen@gmail.com>
2024-03-06 12:11:08 +0000

    Add batch apply for frame

--- nanofold/frame.py
+++ nanofold/frame.py
@@ -48,6 +48,9 @@ class Frame:
 
     @staticmethod
     def apply(frames, vectors):
+        if len(vectors.shape) == 3:
+            b, x, y = vectors.shape
+            vectors = vectors.reshape(b, 1, x, y)
         result = vectors @ frames.rotations.transpose(-2, -1)
         result = result.transpose(-2, -3) + frames.translations
         return result.transpose(-2, -3)
