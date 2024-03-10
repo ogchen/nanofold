Add loss calculation to structure module layer, and fix issue with in place operation in grad computation graph

nanofold/model/invariant_point_attention.py
@@ -95,8 +95,8 @@ class InvariantPointAttention(nn.Module):
         return attention.squeeze(-2)
 
     def frame_attention(self, weight, frames, single_rep):
-        vp = self.value_points(single_rep).permute(1, 2, 0, 3)
-        local_vp = Frame.apply(frames, vp)
+        vp = self.value_points(single_rep)
+        local_vp = Frame.apply(frames, vp.permute(1, 2, 0, 3))
         local_attention = weight.unsqueeze(-2).unsqueeze(-2) @ local_vp
         frames_inverse = Frame.inverse(frames)[:, None, None, ...]
         return Frame.apply(frames_inverse, local_attention.squeeze(-2))

nanofold/model/structure.py
@@ -1,4 +1,5 @@
 from torch import nn
+from nanofold.loss import compute_fape_loss
 from nanofold.model.backbone_update import BackboneUpdate
 from nanofold.frame import Frame
 from nanofold.model.invariant_point_attention import InvariantPointAttention
@@ -42,13 +43,22 @@ class StructureModuleLayer(nn.Module):
     def from_config(cls, config):
         return cls(**cls.get_args(config))
 
-    def forward(self, single, pair, frames):
-        single += self.invariant_point_attention(single, pair, frames)
+    def forward(self, single, pair, frames, frames_truth=None):
+        single = single + self.invariant_point_attention(single, pair, frames)
         single = self.layer_norm1(self.dropout(single))
-        single += self.linear3(self.relu(self.linear2(self.relu(self.linear1(single)))))
+        single = single + self.linear3(self.relu(self.linear2(self.relu(self.linear1(single)))))
         single = self.layer_norm2(self.dropout(single))
         frames = Frame.compose(frames, self.backbone_update(single))
-        return single, frames
+
+        loss = (
+            compute_fape_loss(
+                frames, frames.translations, frames_truth, frames_truth.translations, eps=1e-12
+            )
+            if frames_truth is not None
+            else None
+        )
+
+        return single, frames, loss
 
 
 class StructureModule(nn.Module):

tests/test_structure.py
@@ -1,29 +1,40 @@
 import torch
 from nanofold.frame import Frame
 from nanofold.model.structure import StructureModuleLayer
+class TestStructureModuleLayer:
+    def setup_method(self):
+        self.single_embedding_size = 4
+        self.len_seq = 7
+        self.pair_embedding_size = 5
+        self.model = StructureModuleLayer(
+            single_embedding_size=self.single_embedding_size,
+            pair_embedding_size=self.pair_embedding_size,
+            ipa_embedding_size=6,
+            num_query_points=3,
+            num_value_points=3,
+            num_heads=2,
+            dropout=0.1,
+        )
+        self.len_seq = 7
+        self.single_embedding_size = 4
+        self.pair_embedding_size = 5
+        self.single = torch.rand(self.len_seq, self.single_embedding_size)
+        self.pair = torch.rand(self.len_seq, self.len_seq, self.pair_embedding_size)
+        self.frames = Frame(
+            rotations=torch.eye(3).unsqueeze(0).repeat(self.len_seq, 1, 1),
+            translations=torch.zeros(self.len_seq, 3),
+        )
 
+    def test_structure_module_layer(self):
+        s, f, loss = self.model(self.single, self.pair, self.frames)
+        assert s.shape == self.single.shape
+        assert f.rotations.shape == self.frames.rotations.shape
+        assert f.translations.shape == self.frames.translations.shape
+        assert torch.allclose(f.rotations @ f.rotations.transpose(-2, -1), torch.eye(3), atol=1e-5)
+        assert loss is None
 
-def test_structure_module_layer():
-    single_embedding_size = 4
-    len_seq = 7
-    pair_embedding_size = 5
-    model = StructureModuleLayer(
-        single_embedding_size=single_embedding_size,
-        pair_embedding_size=pair_embedding_size,
-        ipa_embedding_size=6,
-        ipa_num_query_points=3,
-        ipa_num_value_points=3,
-        ipa_num_heads=2,
-        dropout=0.1,
-    )
-    single = torch.rand(len_seq, single_embedding_size)
-    pair = torch.rand(len_seq, len_seq, pair_embedding_size)
-    frames = Frame(
-        rotations=torch.stack([torch.eye(3)] * len_seq), translations=torch.zeros(len_seq, 3)
-    )
-
-    s, f = model(single, pair, frames)
-    assert s.shape == single.shape
-    assert f.rotations.shape == frames.rotations.shape
-    assert f.translations.shape == frames.translations.shape
-    assert torch.allclose(f.rotations @ f.rotations.transpose(-2, -1), torch.eye(3), atol=1e-5)
+    def test_structure_module_layer_loss(self):
+        s, f, loss = self.model(self.single, self.pair, self.frames, self.frames)
+        assert loss is not None
+        # Check no exception raised when we traverse the graph
+        loss.backward()