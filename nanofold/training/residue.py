53787612ca205766c11290115bd01178fd882ea5
Oscar Chen <oscarguoyinchen@gmail.com>
2024-03-13 11:36:53 +0000

    Move compute_residue_frames to data_processing

--- nanofold/training/residue.py
+++ nanofold/training/residue.py
@@ -128,22 +128,6 @@ BACKBONE_POSITIONS = {
 }
 
 
-def compute_residue_frames(coords):
-    x0 = coords[:, 0, :]
-    x1 = coords[:, 1, :]
-    x2 = coords[:, 2, :]
-    v0 = x2 - x1
-    v1 = x0 - x1
-    e0 = v0 / torch.linalg.vector_norm(v0, dim=-1, keepdim=True)
-    dot = e0.unsqueeze(-2) @ v1.unsqueeze(-1)
-    u1 = v1 - e0 * dot.squeeze(-1)
-
-    e1 = u1 / torch.linalg.vector_norm(u1, dim=-1, keepdim=True)
-    e2 = torch.linalg.cross(e0, e1)
-    rotations = torch.stack([e0, e1, e2], dim=-2)
-    return Frame(rotations=rotations, translations=x1)
-
-
 def compute_backbone_coords(frames, sequence):
     if isinstance(sequence, str):
         residue_lookup = dict(RESIDUE_LIST)
--- tests/training/test_residue.py
+++ tests/training/test_residue.py
@@ -2,17 +2,6 @@ from nanofold.training import residue
 import torch
 
 
-def test_compute_residue_frames():
-    num_residues = 5
-    coords = torch.rand(num_residues, 3, 3)
-    frames = residue.compute_residue_frames(coords)
-    frames.rotations
-    assert torch.allclose(
-        frames.rotations @ frames.rotations.transpose(-2, -1), torch.eye(3), atol=1e-5
-    )
-    assert torch.allclose(frames.translations, coords[:, 1, :])
-
-
 def test_compute_backbone_coords():
     sequence = ["MET", "PHE", "PRO", "SER", "THR"]
     sequence_1l = "MFPST"
