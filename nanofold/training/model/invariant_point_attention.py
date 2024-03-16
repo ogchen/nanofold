b3f9fe1d2637a16d053ac0b1f5783d60e11f9995
Oscar Chen <oscarguoyinchen@gmail.com>
2024-03-16 18:07:16 +0000

    Remove util file

--- nanofold/training/model/invariant_point_attention.py
+++ nanofold/training/model/invariant_point_attention.py
@@ -2,7 +2,17 @@ import math
 import torch
 from torch import nn
 from nanofold.training.frame import Frame
-from nanofold.training.util import LinearWithView
+
+
+class LinearWithView(nn.Module):
+    def __init__(self, in_features, out_features, *args, **kwargs):
+        super().__init__()
+        self.linear = nn.Linear(in_features, math.prod(out_features), *args, **kwargs)
+        self.out_features = out_features
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out.view(*out.shape[:-1], *self.out_features)
 
 
 class InvariantPointAttention(nn.Module):
