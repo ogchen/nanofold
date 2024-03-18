f7dfa8210f6ec85ab3ea816888a77628974d4c79
Oscar Chen <oscarguoyinchen@gmail.com>
2024-03-18 13:53:25 +0000

    Create logger and mlflow loggers

---
+++ nanofold/training/logging/logger.py
@@ -0,0 +1,17 @@
+import logging
+import torchinfo
+
+
+class Logger:
+    def log_model_summary(self, model):
+        logging.info(str(torchinfo.summary(model, verbose=0)))
+
+    def log_epoch(self, epoch, metrics):
+        metrics_str = ",".join([f"{k}: {metric}" for k, metric in metrics.items()])
+        logging.info(f"Epoch {epoch}: {metrics_str}")
+
+    def log_model(self, model):
+        pass
+
+    def log_params(self, params):
+        pass
---
+++ nanofold/training/logging/mlflow.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import mlflow
+import tempfile
+import torchinfo
+
+from nanofold.training.logging import Logger
+
+
+class MLFlowLogger(Logger):
+    def __init__(self, uri, pip_requirements):
+        super().__init__()
+        self.pip_requirements = pip_requirements
+        mlflow.set_tracking_uri(uri=uri)
+        mlflow.start_run()
+
+    def __del__(self):
+        mlflow.end_run()
+
+    def log_model_summary(self, model):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            model_summary = Path(tmp_dir) / "model_summary.txt"
+            model_summary.write_text(str(torchinfo.summary(model, verbose=0)))
+            mlflow.log_artifact(model_summary)
+
+    def log_epoch(self, epoch, metrics):
+        for k, metric in metrics.items():
+            mlflow.log_metric(k, metric, step=epoch)
+
+    def log_model(self, model):
+        mlflow.pytorch.log_model(model, "model", pip_requirements=self.pip_requirements)
+
+    def log_params(self, params):
+        mlflow.log_params(params)
