63df285b938d6c54cddd80a13b51c6316f3d1d74
Oscar Chen <oscarguoyinchen@gmail.com>
2024-04-07 10:44:40 +0000

    Remove polars and directly use pyarrow

--- nanofold/training/chain_dataset.py
+++ nanofold/training/chain_dataset.py
@@ -2,7 +2,8 @@ from torch.utils.data import IterableDataset
 import logging
 import math
 import numpy as np
-import polars as pl
+import pyarrow as pa
+import pyarrow.compute as pc
 import random
 import torch
 
@@ -17,7 +18,7 @@ SAMPLE_SIZE = 100
 
 
 def accept_chain(row):
-    return np.random.rand() < max(min(row["length"], 512), 256) / 512
+    return np.random.rand() < max(min(row.length, 512), 256) / 512
 
 
 def encode_one_hot(seq):
@@ -38,7 +39,7 @@ def encode_one_hot_alignments(alignments):
 
 
 def encode_deletion_matrix(deletion_matrix):
-    counts = torch.tensor(deletion_matrix, dtype=torch.float32)
+    counts = torch.from_numpy(np.vstack(deletion_matrix))
     has_deletion = counts > 0
     deletion_value = 2 / math.pi * torch.arctan(counts / 3)
     return torch.cat((has_deletion.unsqueeze(-1), deletion_value.unsqueeze(-1)), dim=-1)
@@ -51,12 +52,12 @@ def encode_msa(msa):
 
 
 def preprocess_msa(msa, num_msa):
+    msa = zip(*msa.values())
     msa = [m for m in msa if not all(c == MSA_GAP for c in m[0])]
     query = msa[0]
 
     # Deduplicate MSA
-    hashable_msa = [(a, tuple(d)) for a, d in msa[1:]]
-    deduplicated = [(a, list(d)) for a, d in set(hashable_msa)]
+    deduplicated = list(set([(a, torch.tensor(d)) for a, d in msa[1:]]))
 
     # Shuffle MSA
     random.shuffle(deduplicated)
@@ -64,74 +65,87 @@ def preprocess_msa(msa, num_msa):
     # Pad or truncate MSA
     msa = [query, *[m for m in deduplicated if m[0] != query]][:num_msa]
     for _ in range(num_msa - len(msa)):
-        msa.append((MSA_GAP * len(query[0]), [0] * len(query[1])))
+        msa.append((MSA_GAP * len(query[0]), torch.zeros(len(query[1]), dtype=torch.int32)))
 
     return msa
 
 
 class ChainDataset(IterableDataset):
-    def __init__(self, df, indices, residue_crop_size, num_msa):
+    def __init__(self, table, indices, residue_crop_size, num_msa):
         super().__init__()
         self.residue_crop_size = residue_crop_size
         self.num_msa = num_msa
-        self.df = df.with_row_count("index").with_columns(length=pl.col("sequence").str.len_chars())
+        self.table = table
         self.indices = indices
 
     @classmethod
     def construct_datasets(cls, features_file, train_split, *args, **kwargs):
-        df = pl.read_ipc(
-            features_file, columns=["rotations", "translations", "sequence", "positions", "msa"]
-        )
-        logging.info(f"Dataframe loaded, estimated size {df.estimated_size(unit='mb'):.2f} MB")
-        train_size = int(train_split * len(df))
-        if train_size <= 0 or train_split >= len(df):
+        with pa.memory_map(str(features_file)) as source:
+            with pa.ipc.open_file(source) as reader:
+                table = reader.read_all()
+        table_size = table.get_total_buffer_size() / (1024**2)
+        logging.info(f"Features table loaded, size {table_size:.2f} MB")
+        train_size = int(train_split * table.num_rows)
+        if train_size <= 0 or train_size >= table.num_rows:
             raise ValueError(f"train_size must be between 0 and len(df), got {train_size}")
-        indices = np.arange(len(df))
+        indices = np.arange(table.num_rows)
         np.random.shuffle(indices)
-        return cls(df, indices[:train_size], *args, **kwargs), cls(
-            df, indices[train_size:], *args, **kwargs
+        table = table.append_column(
+            "index",
+            [np.arange(table.num_rows)],
+        ).append_column(
+            "length",
+            pc.list_value_length(table["positions"]),
+        )
+        return cls(table, indices[:train_size], *args, **kwargs), cls(
+            table, indices[train_size:], *args, **kwargs
         )
 
     def __iter__(self):
+        slice_column = lambda col_name: lambda r: r[col_name][
+            r["start"] : r["start"] + self.residue_crop_size
+        ]
+        slice_column_msa = lambda r: {
+            "alignments": [
+                a[r["start"] : r["start"] + self.residue_crop_size] for a in r["msa"]["alignments"]
+            ],
+            "deletion_matrix": [
+                d[r["start"] : r["start"] + self.residue_crop_size]
+                for d in r["msa"]["deletion_matrix"]
+            ],
+        }
+
         while True:
             sampled_indices = np.random.choice(self.indices, SAMPLE_SIZE)
-            sample = self.df.filter(pl.col("index").is_in(sampled_indices)).filter(
-                pl.col("length") >= self.residue_crop_size
+            expression = pc.field("index").isin(sampled_indices) & (
+                pc.field("length") >= self.residue_crop_size
             )
-            sample = sample.with_columns(
-                start=pl.lit(np.random.randint(sample["length"] - self.residue_crop_size + 1)),
-            ).with_columns(
-                positions=pl.col("positions").list.slice(pl.col("start"), self.residue_crop_size),
-                sequence=pl.col("sequence").str.slice(pl.col("start"), self.residue_crop_size),
-                translations=pl.col("translations").list.slice(
-                    pl.col("start"), self.residue_crop_size
-                ),
-                rotations=pl.col("rotations").list.slice(pl.col("start"), self.residue_crop_size),
+            sample = self.table.filter(expression)
+            df = sample.to_pandas(use_threads=False)
+            df["start"] = df["length"].apply(
+                lambda x: np.random.randint(x - self.residue_crop_size + 1)
             )
+            for col_name in ["positions", "sequence", "translations", "rotations"]:
+                df[col_name] = df.apply(slice_column(col_name), axis=1)
+            df["msa"] = df.apply(slice_column_msa, axis=1)
 
-            for row in sample.iter_rows(named=True):
+            for row in df.itertuples():
                 if accept_chain(row):
                     yield self.parse_features(row)
 
-    def parse_msa_features(self, row):
-        msa = [
-            (
-                a[row["start"] : row["start"] + self.residue_crop_size],
-                d[row["start"] : row["start"] + self.residue_crop_size],
-            )
-            for a, d in zip(row["msa"]["alignments"], row["msa"]["deletion_matrix"])
-        ]
+    def parse_msa_features(self, msa):
         return encode_msa(preprocess_msa(msa, self.num_msa))
 
     def parse_features(self, row):
+        pass
         features = {
-            "rotations": torch.tensor(row["rotations"]),
-            "translations": torch.tensor(row["translations"]),
+            "rotations": torch.from_numpy(np.stack(np.vstack(row.rotations.tolist()).tolist())),
+            "translations": torch.from_numpy(np.vstack(row.translations.tolist())),
             "local_coords": torch.tensor(
-                [[p[1] for p in get_atom_positions(r)] for r in row["sequence"]]
+                [[p[1] for p in get_atom_positions(r)] for r in row.sequence]
             ),
-            "target_feat": encode_one_hot(row["sequence"]),
-            "msa_feat": self.parse_msa_features(row),
-            "positions": torch.tensor(row["positions"]),
+            "target_feat": encode_one_hot(row.sequence),
+            "msa_feat": self.parse_msa_features(row.msa),
+            "positions": torch.from_numpy(row.positions),
         }
         return features
