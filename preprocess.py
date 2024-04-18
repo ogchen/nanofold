197f2696ae3a9a1b69a645eee747922b52ec2c54
Oscar Chen <oscarguoyinchen@gmail.com>
2024-04-18 10:58:01 +0100

    Add building of templates to preprocessing script

--- preprocess.py
+++ preprocess.py
@@ -7,10 +7,12 @@ from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
 from nanofold.data_processing.db import DBManager
+from nanofold.data_processing.hhsearch import HHSearchRunner
 from nanofold.data_processing.ipc import dump_to_ipc
 from nanofold.data_processing.mmcif_processor import process_mmcif_files
 from nanofold.data_processing.msa_builder import build_msa
 from nanofold.data_processing.msa_runner import MSARunner
+from nanofold.data_processing.template import build_template
 
 
 def parse_args():
@@ -21,6 +23,7 @@ def parse_args():
     parser.add_argument("-m", "--mmcif", help="Directory containing mmcif files", type=Path)
     parser.add_argument("-o", "--output", help="Directory to store output files", type=Path)
     parser.add_argument("-s", "--small_bfd", help="Small BFD file", type=Path)
+    parser.add_argument("-p", "--pdb70", help="PDB70 file", type=Path)
     parser.add_argument("--dump-only", help="Dump IPC file only", action="store_true")
     parser.add_argument("-l", "--logging", help="Logging level", default="INFO")
     return parser.parse_args()
@@ -38,6 +41,8 @@ def main():
     ipc_output_path = args.output / "features.arrow"
     jackhmmer_results_path = args.output / "small_bfd_cache"
     jackhmmer_results_path.mkdir(exist_ok=True)
+    template_cache_dir = args.output / "templates_cache"
+    template_cache_dir.mkdir(exist_ok=True)
 
     db_manager = DBManager(uri=os.getenv("MONGODB_URI"))
     msa_runner = MSARunner(
@@ -47,14 +52,27 @@ def main():
         num_cpus=1,
         max_sequences=500,
     )
+    hhsearch_runner = HHSearchRunner(
+        shutil.which("hhsearch"),
+        args.pdb70,
+        template_cache_dir,
+    )
 
-    if not args.dump_only:
-        with ProcessPoolExecutor() as executor:
-            process_mmcif_files(db_manager, executor, args.mmcif, args.batch)
+    # if not args.dump_only:
+    # with ProcessPoolExecutor() as executor:
+    # process_mmcif_files(db_manager, executor, args.mmcif, args.batch)
 
     with ThreadPoolExecutor() as executor:
         if not args.dump_only:
             build_msa(msa_runner, db_manager, executor, msa_output_dir)
+            build_template(
+                hhsearch_runner,
+                shutil.which("reformat.pl"),
+                msa_runner,
+                db_manager,
+                executor,
+                msa_output_dir,
+            )
         dump_to_ipc(db_manager, msa_output_dir, ipc_output_path, executor)
 
 
