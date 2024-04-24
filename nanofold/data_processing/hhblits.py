f952d64f0074882538133b6b637e36bd9500297e
Oscar Chen <oscarguoyinchen@gmail.com>
2024-04-24 17:16:38 +0000

    Add extra parameters to HHblits runner for MSA search

--- nanofold/data_processing/hhblits.py
+++ nanofold/data_processing/hhblits.py
@@ -5,27 +5,38 @@ from pathlib import Path
 
 
 class HHblitsRunner:
-    def __init__(self, bin, pdb70, cache_dir):
+    def __init__(
+        self, bin, db, cache_dir, num_iterations, output_format="hhr", num_cpu=1, **kwargs
+    ):
         self.bin = bin
-        self.pdb70 = pdb70
+        self.db = db
         self.cache_dir = cache_dir
+        self.num_iterations = num_iterations
+        self.num_cpu = num_cpu
+        self.output_format = output_format
+        self.kwargs = kwargs
 
     def build_cmd(self, a2m_file, output):
-        return [
+        cmd = [
             self.bin,
             "-i",
             a2m_file,
-            "-o",
-            output,
             "-d",
-            self.pdb70,
+            self.db,
             "-v",
             "0",
             "-cpu",
-            "1",
+            self.num_cpu,
             "-n",
-            "1",
+            self.num_iterations,
         ]
+        if self.output_format == "hhr":
+            cmd.extend(["-o", output])
+        elif self.output_format == "a3m":
+            cmd.extend(["-oa3m", output])
+        for k, v in self.kwargs.items():
+            cmd.extend([f"-{k}", v])
+        return [str(c) for c in cmd]
 
     def cached_result(self, output):
         zip_output = Path(f"{output}.gz")
@@ -43,7 +54,7 @@ class HHblitsRunner:
         return content
 
     def run(self, a2m_file, id):
-        output = self.cache_dir / f"{id}.hhr"
+        output = self.cache_dir / f"{id}.{self.output_format}"
         cached_result = self.cached_result(output)
         if cached_result is not None:
             return cached_result
