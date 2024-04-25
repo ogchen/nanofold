d06d546ca8a65ab181601da6aab6cec218342d0e
Oscar Chen <oscarguoyinchen@gmail.com>
2024-04-25 15:27:55 +0100

    Fix failing sto test

--- tests/data_processing/test_sto_parser.py
+++ tests/data_processing/test_sto_parser.py
@@ -1,6 +1,6 @@
 from io import StringIO
 
-from nanofold.data_processing.sto_parser import parse_msa
+from nanofold.data_processing.sto_parser import extract_alignments
 
 
 def test_parse_msa():
@@ -19,7 +19,7 @@ Q8QVG3_ADEBA/907-1142              -----LALALGSGLAVN---------S-NN
 //
         """
     )
-    alignments, deletion_matrix = parse_msa(input)
+    alignments, deletion_matrix = extract_alignments(input)
     expected_deletion_matrix = [[0] * 21 for _ in range(4)]
     expected_deletion_matrix[2][17] = 5
     expected_deletion_matrix[2][19] = 1
@@ -31,10 +31,3 @@ Q8QVG3_ADEBA/907-1142              -----LALALGSGLAVN---------S-NN
         "-----LALALGSGLAVN--NN",
     ]
     assert deletion_matrix == expected_deletion_matrix
-
-    num_samples = 2
-    sampled_alignments, sampled_deletion = parse_msa(input, num_samples)
-    assert alignments[0] == sampled_alignments[0]
-    assert deletion_matrix[0] == sampled_deletion[0]
-    assert len(sampled_alignments) == num_samples
-    assert len(sampled_deletion) == num_samples
