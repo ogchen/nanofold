cf5a30494cf16db8bdfc624b5f431d02bcabd157
Oscar Chen <oscarguoyinchen@gmail.com>
2024-05-09 14:52:36 +0000

    Correct implementation of diffusion transformer blocks

--- nanofold/training/model/diffusion_transformer.py
+++ nanofold/training/model/diffusion_transformer.py
@@ -4,11 +4,10 @@ from nanofold.training.model.attention_pair_bias import AttentionPairBias
 from nanofold.training.model.conditioned_transition_block import ConditionedTransitionBlock
 
 
-class DiffusionTransformer(nn.Module):
-    def __init__(self, atom_embedding_size, atom_pair_embedding_size, num_block, num_head):
+class DiffusionTransformerBlock(nn.Module):
+    def __init__(self, atom_embedding_size, atom_pair_embedding_size, num_head):
+
         super().__init__()
-        self.num_block = num_block
-        self.num_head = num_head
         self.attention_pair_bias = AttentionPairBias(
             num_head, atom_embedding_size, atom_embedding_size, atom_pair_embedding_size
         )
@@ -17,7 +16,22 @@ class DiffusionTransformer(nn.Module):
         )
 
     def forward(self, a, s, pair_rep, beta):
-        for _ in range(self.num_block):
-            b = self.attention_pair_bias(a, s, pair_rep, beta)
-            a = b + self.conditioned_transition_block(a, s)
+        b = self.attention_pair_bias(a, s, pair_rep, beta)
+        a = b + self.conditioned_transition_block(a, s)
+        return a
+
+
+class DiffusionTransformer(nn.Module):
+    def __init__(self, atom_embedding_size, atom_pair_embedding_size, num_block, num_head):
+        super().__init__()
+        self.blocks = nn.ModuleList(
+            [
+                DiffusionTransformerBlock(atom_embedding_size, atom_pair_embedding_size, num_head)
+                for _ in range(num_block)
+            ]
+        )
+
+    def forward(self, a, s, pair_rep, beta):
+        for block in self.blocks:
+            a = block(a, s, pair_rep, beta)
         return a
