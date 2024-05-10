6b66b0b09e41fe8baba46e28a8474e0a05d062e3
Oscar Chen <oscarguoyinchen@gmail.com>
2024-05-10 22:29:07 +0100

    Add rigid align function

--- nanofold/training/util.py
+++ nanofold/training/util.py
@@ -17,3 +17,19 @@ def uniform_random_rotation(*batch_size):
     quaternion = torch.rand(*batch_size, 3) * 2 if batch_size else torch.rand(1, 3) * 2
     rotation = quaternion_to_rotation_matrix(quaternion)
     return rotation if batch_size else rotation[0]
+
+
+def rigid_align(x, x_truth):
+    x_mean = x.mean(dim=-2, keepdim=True)
+    x = x - x_mean
+    x_truth = x_truth - x_truth.mean(dim=-2, keepdim=True)
+    product = torch.einsum("...la,...lb->...ab", x, x_truth)
+    U, _, V = torch.svd(product.float())
+    R = U @ V
+    is_reflection = torch.linalg.det(R.float()).unsqueeze(-1).unsqueeze(-1) < 0
+    R = (
+        R * ~is_reflection
+        + (U @ torch.diag(torch.tensor([1.0, 1.0, -1.0], device=R.device)) @ V) * is_reflection
+    )
+    x_align = (R @ x.transpose(-2, -1)).transpose(-2, -1) + x_mean
+    return x_align
