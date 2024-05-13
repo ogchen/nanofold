0a810a7ae84acfc0192b8877a4bb5cd47e1c7349
Oscar Chen <oscarguoyinchen@gmail.com>
2024-05-13 22:14:27 +0100

    Implement nanofold model based on Alphafold 3

--- nanofold/training/model/nanofold.py
+++ nanofold/training/model/nanofold.py
@@ -1,238 +1,233 @@
-import numpy as np
 import torch
 from torch import nn
 
-from nanofold.training.frame import Frame
 from nanofold.training.loss import DistogramLoss
-from nanofold.training.model.evoformer import Evoformer
-from nanofold.training.model.extra_msa import ExtraMSAStack
+from nanofold.training.model.diffusion_model import DiffusionModel
 from nanofold.training.model.input import InputEmbedding
-from nanofold.training.model.masked_msa import MaskedMSAPredictor
-from nanofold.training.model.recycle import RecyclingEmbedder
-from nanofold.training.model.structure import StructureModule
-from nanofold.training.model.template import TemplatePairStack
-from nanofold.training.model.template import TemplatePointwiseAttention
+from nanofold.training.model.msa_module import MSAModule
+from nanofold.training.model.pairformer import Pairformer
+from nanofold.training.model.relative_position_encoding import RelativePositionEncoding
+from nanofold.training.model.template_embedder import TemplateEmbedder
 
 
 class Nanofold(nn.Module):
     def __init__(
         self,
+        device,
+        compile_model,
+        use_grad_checkpoint,
         num_recycle,
-        num_structure_layers,
         single_embedding_size,
         pair_embedding_size,
+        input_atom_embedding_size,
+        input_atom_pair_embedding_size,
+        input_token_embedding_size,
+        position_bins,
+        num_atom_transformer_blocks,
+        num_atom_transformer_heads,
+        num_atom_transformer_queries,
+        num_atom_transformer_keys,
+        product_embedding_size,
+        num_msa_samples,
+        num_msa_blocks,
         msa_embedding_size,
-        extra_msa_embedding_size,
+        msa_averaging_embedding_size,
+        num_msa_heads,
+        msa_transition_multiplier,
         num_triangular_update_channels,
         num_triangular_attention_channels,
-        product_embedding_size,
-        position_bins,
-        template_embedding_size,
-        num_template_channels,
-        num_template_heads,
+        num_triangular_attention_heads,
         num_template_blocks,
-        template_transition_multiplier,
-        num_extra_msa_blocks,
-        num_extra_msa_channels,
-        num_evoformer_blocks,
-        num_evoformer_msa_heads,
-        num_evoformer_pair_heads,
-        num_evoformer_channels,
-        evoformer_transition_multiplier,
-        structure_dropout,
-        ipa_embedding_size,
-        num_ipa_query_points,
-        num_ipa_value_points,
-        num_ipa_heads,
+        template_embedding_size,
+        num_pairformer_blocks,
+        num_pair_heads,
+        pairformer_transition_multiplier,
+        diffusion_steps,
+        diffusion_batch_size,
+        atom_embedding_size,
+        atom_pair_embedding_size,
+        token_embedding_size,
+        num_diffusion_transformer_blocks,
+        num_diffusion_transformer_heads,
+        fourier_embedding_size,
         num_distogram_bins,
-        num_lddt_bins,
-        num_lddt_channels,
-        use_grad_checkpoint,
-        device,
     ):
         super().__init__()
+
         self.device = device
+        self.use_grad_checkpoint = use_grad_checkpoint
         self.num_recycle = num_recycle
-        self.msa_embedding_size = msa_embedding_size
-        self.pair_embedding_size = pair_embedding_size
-        self.input_embedder = InputEmbedding(pair_embedding_size, msa_embedding_size, position_bins)
-        self.recycling_embedder = RecyclingEmbedder(pair_embedding_size, msa_embedding_size, device)
-        self.template_pair_stack = TemplatePairStack(
+        self.input_embedder = InputEmbedding(
+            input_atom_embedding_size,
+            input_atom_pair_embedding_size,
+            input_token_embedding_size,
+            single_embedding_size,
+            pair_embedding_size,
+            num_atom_transformer_blocks,
+            num_atom_transformer_heads,
+            num_atom_transformer_queries,
+            num_atom_transformer_keys,
+        )
+        input_embedding_size = self.input_embedder.embedding_size
+        self.linear_input_single = nn.Linear(input_embedding_size, single_embedding_size)
+        self.linear_input_pair_a = nn.Linear(input_embedding_size, pair_embedding_size)
+        self.linear_input_pair_b = nn.Linear(input_embedding_size, pair_embedding_size)
+        self.relative_position_encoding = RelativePositionEncoding(
+            position_bins, pair_embedding_size
+        )
+        self.transition_pair = nn.Sequential(
+            nn.LayerNorm(pair_embedding_size),
+            nn.Linear(pair_embedding_size, pair_embedding_size, bias=False),
+        )
+        self.template_embedder = TemplateEmbedder(
             template_embedding_size,
-            num_template_channels,
-            num_template_heads,
+            pair_embedding_size,
+            num_triangular_update_channels,
+            num_triangular_attention_channels,
+            num_triangular_attention_heads,
+            num_pair_heads,
+            pairformer_transition_multiplier,
             num_template_blocks,
-            template_transition_multiplier,
-            device,
         )
-        self.template_pointwise_attention = TemplatePointwiseAttention(
-            pair_embedding_size,
-            template_embedding_size,
-            num_template_heads,
-            num_template_channels,
-        )
-        self.extra_msa_stack = ExtraMSAStack(
-            pair_embedding_size,
-            extra_msa_embedding_size,
-            num_triangular_update_channels,
-            num_triangular_attention_channels,
-            product_embedding_size,
-            num_extra_msa_blocks,
-            num_evoformer_msa_heads,
-            num_evoformer_pair_heads,
-            num_extra_msa_channels,
-            evoformer_transition_multiplier,
-            device,
-        )
-        self.evoformer = Evoformer(
-            single_embedding_size,
-            pair_embedding_size,
+        self.msa_module = MSAModule(
+            num_msa_blocks,
+            input_embedding_size,
             msa_embedding_size,
+            num_msa_samples,
+            pair_embedding_size,
+            msa_averaging_embedding_size,
+            product_embedding_size,
             num_triangular_update_channels,
             num_triangular_attention_channels,
-            product_embedding_size,
-            num_evoformer_blocks,
-            num_evoformer_msa_heads,
-            num_evoformer_pair_heads,
-            num_evoformer_channels,
-            evoformer_transition_multiplier,
-            device,
+            num_msa_heads,
+            num_pair_heads,
+            msa_transition_multiplier,
         )
-        self.structure_module = StructureModule(
-            num_structure_layers,
+        self.transition_single = nn.Sequential(
+            nn.LayerNorm(single_embedding_size),
+            nn.Linear(single_embedding_size, single_embedding_size, bias=False),
+        )
+        self.pairformer = Pairformer(
             single_embedding_size,
             pair_embedding_size,
-            structure_dropout,
-            ipa_embedding_size,
-            num_ipa_query_points,
-            num_ipa_value_points,
-            num_ipa_heads,
-            num_lddt_bins,
-            num_lddt_channels,
-            device,
+            num_triangular_update_channels,
+            num_triangular_attention_channels,
+            num_triangular_attention_heads,
+            num_pair_heads,
+            pairformer_transition_multiplier,
+            num_pairformer_blocks,
+        )
+        self.diffusion_model = DiffusionModel(
+            diffusion_batch_size,
+            diffusion_steps,
+            atom_embedding_size,
+            atom_pair_embedding_size,
+            token_embedding_size,
+            input_embedding_size,
+            single_embedding_size,
+            pair_embedding_size,
+            fourier_embedding_size,
+            num_atom_transformer_blocks,
+            num_atom_transformer_heads,
+            num_atom_transformer_queries,
+            num_atom_transformer_keys,
+            num_diffusion_transformer_blocks,
+            num_diffusion_transformer_heads,
+            position_bins,
         )
         self.distogram_loss = DistogramLoss(pair_embedding_size, num_distogram_bins, device)
-        self.msa_predictor = MaskedMSAPredictor(msa_embedding_size)
-        self.use_grad_checkpoint = use_grad_checkpoint
+        self.compile(compile_model)
+
+    def compile(self, compile_model):
+        self.pairformer = torch.compile(
+            self.pairformer,
+            disable=not compile_model,
+            dynamic=True,
+        )
 
     @staticmethod
     def get_args(config):
         return {
+            "device": config["device"],
+            "compile_model": config["compile_model"],
+            "use_grad_checkpoint": config["use_grad_checkpoint"],
             "num_recycle": config["num_recycle"],
-            "num_structure_layers": config["num_structure_layers"],
             "single_embedding_size": config["single_embedding_size"],
             "pair_embedding_size": config["pair_embedding_size"],
-            "msa_embedding_size": config["msa_embedding_size"],
-            "extra_msa_embedding_size": config["extra_msa_embedding_size"],
+            "input_atom_embedding_size": config["input_atom_embedding_size"],
+            "input_atom_pair_embedding_size": config["input_atom_pair_embedding_size"],
+            "input_token_embedding_size": config["input_token_embedding_size"],
             "position_bins": config["position_bins"],
+            "num_atom_transformer_blocks": config["num_atom_transformer_blocks"],
+            "num_atom_transformer_heads": config["num_atom_transformer_heads"],
+            "num_atom_transformer_queries": config["num_atom_transformer_queries"],
+            "num_atom_transformer_keys": config["num_atom_transformer_keys"],
+            "product_embedding_size": config["product_embedding_size"],
+            "num_msa_samples": config["num_msa_samples"],
+            "num_msa_blocks": config["num_msa_blocks"],
+            "msa_embedding_size": config["msa_embedding_size"],
+            "msa_averaging_embedding_size": config["msa_averaging_embedding_size"],
+            "num_msa_heads": config["num_msa_heads"],
+            "msa_transition_multiplier": config["msa_transition_multiplier"],
             "num_triangular_update_channels": config["num_triangular_update_channels"],
             "num_triangular_attention_channels": config["num_triangular_attention_channels"],
-            "product_embedding_size": config["product_embedding_size"],
-            "template_embedding_size": config["template_embedding_size"],
-            "num_template_channels": config["num_template_channels"],
-            "num_template_heads": config["num_template_heads"],
+            "num_triangular_attention_heads": config["num_triangular_attention_heads"],
             "num_template_blocks": config["num_template_blocks"],
-            "template_transition_multiplier": config["template_transition_multiplier"],
-            "num_extra_msa_blocks": config["num_extra_msa_blocks"],
-            "num_extra_msa_channels": config["num_extra_msa_channels"],
-            "num_evoformer_blocks": config["num_evoformer_blocks"],
-            "num_evoformer_msa_heads": config["num_evoformer_msa_heads"],
-            "num_evoformer_pair_heads": config["num_evoformer_pair_heads"],
-            "num_evoformer_channels": config["num_evoformer_channels"],
-            "evoformer_transition_multiplier": config["evoformer_transition_multiplier"],
-            "structure_dropout": config["structure_dropout"],
-            "ipa_embedding_size": config["ipa_embedding_size"],
-            "num_ipa_query_points": config["num_ipa_query_points"],
-            "num_ipa_value_points": config["num_ipa_value_points"],
-            "num_ipa_heads": config["num_ipa_heads"],
+            "template_embedding_size": config["template_embedding_size"],
+            "num_pairformer_blocks": config["num_pairformer_blocks"],
+            "num_pair_heads": config["num_pair_heads"],
+            "pairformer_transition_multiplier": config["pairformer_transition_multiplier"],
+            "diffusion_steps": config["diffusion_steps"],
+            "diffusion_batch_size": config["diffusion_batch_size"],
+            "atom_embedding_size": config["atom_embedding_size"],
+            "atom_pair_embedding_size": config["atom_pair_embedding_size"],
+            "token_embedding_size": config["token_embedding_size"],
+            "num_diffusion_transformer_blocks": config["num_diffusion_transformer_blocks"],
+            "num_diffusion_transformer_heads": config["num_diffusion_transformer_heads"],
+            "fourier_embedding_size": config["fourier_embedding_size"],
             "num_distogram_bins": config["num_distogram_bins"],
-            "num_lddt_bins": config["num_lddt_bins"],
-            "num_lddt_channels": config["num_lddt_channels"],
-            "use_grad_checkpoint": config["use_grad_checkpoint"],
-            "device": config["device"],
         }
 
     @classmethod
     def from_config(cls, config):
         return cls(**cls.get_args(config))
 
-    def run_evoformer(self, *args):
-        if self.use_grad_checkpoint or not self.training:
+    def run_pairformer(self, *args):
+        if self.use_grad_checkpoint:
             return torch.utils.checkpoint.checkpoint(
-                lambda *inputs: self.evoformer(*inputs), *args, use_reentrant=False
+                lambda *inputs: self.pairformer(*inputs), *args, use_reentrant=False
             )
-        return self.evoformer(*args)
+        return self.pairformer(*args)
 
-    def get_total_loss(self, fape_loss, conf_loss, aux_loss, dist_loss, msa_loss):
-        return 0.5 * fape_loss + 0.5 * aux_loss + 0.01 * conf_loss + 0.6 * dist_loss + 2 * msa_loss
+    def get_total_loss(self, diffusion_loss, dist_loss):
+        return 4 * diffusion_loss + 0.03 * dist_loss
 
-    def forward(self, batch):
+    def forward(self, features):
         num_recycle = (
             torch.randint(self.num_recycle, (1,)) + 1 if self.training else self.num_recycle
         )
-        fape_clamp = 10.0 if torch.rand(1) < 0.9 and self.training else None
-        template_pair_feat = batch["template_pair_feat"][..., :4, :, :, :]
 
-        s = batch["positions"].shape
-        prev_msa_row = torch.zeros((*s, self.msa_embedding_size), device=self.device)
-        prev_ca_coords = torch.zeros((*s, 3), device=self.device)
-        prev_pair_rep = torch.zeros((*s, s[-1], self.pair_embedding_size), device=self.device)
-
-        for i in range(num_recycle):
-            prev_msa_row = prev_msa_row.detach()
-            prev_ca_coords = prev_ca_coords.detach()
-            prev_pair_rep = prev_pair_rep.detach()
-
-            msa_rep, pair_rep = self.input_embedder(
-                batch["target_feat"], batch["positions"], batch["msa_feat"]
-            )
-            msa_row_update, pair_rep_update = self.recycling_embedder(
-                prev_msa_row, prev_pair_rep, prev_ca_coords
-            )
-            msa_rep[..., 0, :, :] = msa_rep[..., 0, :, :] + msa_row_update
-            pair_rep = pair_rep + pair_rep_update
-
-            template_rep = self.template_pair_stack(template_pair_feat)
-            pair_rep = pair_rep + self.template_pointwise_attention(template_rep, pair_rep)
-
-            pair_rep = self.extra_msa_stack(batch["extra_msa_feat"], pair_rep)
-
-            msa_rep, pair_rep, single_rep = self.run_evoformer(msa_rep, pair_rep)
-
-            coords, chain_plddt, chain_lddt, fape_loss, conf_loss, aux_loss = self.structure_module(
-                single_rep,
-                pair_rep,
-                batch["local_coords"],
-                (
-                    Frame(
-                        rotations=batch["rotations"],
-                        translations=batch["translations"],
-                    )
-                    if i == num_recycle - 1 and "translations" in batch
-                    else None
-                ),
-                fape_clamp,
-            )
-            prev_msa_row = msa_rep[..., 0, :, :]
-            prev_pair_rep = pair_rep
-            prev_ca_coords = coords[..., 1, :]
-
-        msa_loss = self.msa_predictor(msa_rep, batch["cluster_mask"], batch["masked_msa_truth"])
-
-        dist_loss = (
-            self.distogram_loss(pair_rep, batch["translations"])
-            if "translations" in batch
-            else None
+        input = self.input_embedder(features)
+        single_rep_init = self.linear_input_single(input)
+        pair_rep_init = self.linear_input_pair_a(input.unsqueeze(-2)) + self.linear_input_pair_b(
+            input.unsqueeze(-3)
         )
+        pair_rep_init = pair_rep_init + self.relative_position_encoding(features["residue_index"])
+        single_rep_prev = torch.zeros_like(single_rep_init)
+        pair_rep_prev = torch.zeros_like(pair_rep_init)
 
+        for _ in range(num_recycle):
+            pair_rep = pair_rep_init + self.transition_pair(pair_rep_prev)
+            pair_rep = pair_rep + self.template_embedder(features, pair_rep)
+            pair_rep = pair_rep + self.msa_module(features, pair_rep, input)
+            single_rep = single_rep_init + self.transition_single(single_rep_prev)
+            single_rep, pair_rep = self.run_pairformer(single_rep, pair_rep)
+            single_rep_prev, pair_rep_prev = single_rep, pair_rep
+
+        diffusion_loss = self.diffusion_model(features, input, single_rep, pair_rep)
+        dist_loss = self.distogram_loss(pair_rep, features["translations"])
         return {
-            "coords": coords,
-            "chain_plddt": chain_plddt.mean(),
-            "chain_lddt": chain_lddt.mean(),
-            "fape_loss": fape_loss,
-            "conf_loss": conf_loss,
-            "aux_loss": aux_loss,
+            "diffusion_loss": diffusion_loss,
             "dist_loss": dist_loss,
-            "msa_loss": msa_loss,
-            "total_loss": self.get_total_loss(fape_loss, conf_loss, aux_loss, dist_loss, msa_loss),
+            "total_loss": self.get_total_loss(diffusion_loss, dist_loss),
         }
