718f2ccc2ad008408ad32415d61e91cf416be215
Oscar Chen <oscarguoyinchen@gmail.com>
2024-05-14 17:53:01 +0100

    Fix hhr parser test

--- tests/data_processing/test_hhr_parser.py
+++ tests/data_processing/test_hhr_parser.py
@@ -3,7 +3,7 @@ from nanofold.data_processing.hhr_parser import parse_hhr
 
 def test_parse_hhr(data_dir):
     with open(data_dir / "1vh2_A.hhr") as f:
-        result = parse_hhr(f.read())
+        result = parse_hhr(f.readlines())
     assert len(result) == 37
     for r in result.values():
         assert r["query_start_pos"] < r["query_end_pos"]
